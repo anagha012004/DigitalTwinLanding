6f2c26f44afb8e351a5ae2ad8269c603166869bd
Om Kadu <kaduom444@gmail.com>
2025-08-25 23:22:54 +0530

    tailwind setup done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + Reactddd</h1>
+      <h1 className='text-red-700 font-bold text-3xl'>Vite + Reactddd</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
